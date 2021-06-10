11472b836a228d71f1bf435b194fb1a8ffe96588
LeeHyungGeol <rjf1138@naver.com>
2021-06-11 00:50:20 +0900

    2021-06-11 / 2020 카카오 공채 : 괄호 변환 : DFS, 구현

--- [프로그래머스 레벨 2] 괄호 변환.cpp
+++ [프로그래머스 레벨 2] 괄호 변환.cpp
@@ -1,18 +1,79 @@
 //https://jhnyang.tistory.com/116
 
-#include <iostream>
+// DFS, 구현이라고 볼 수 있는 문제
+// 구현에 조금 더 가까우나 DFS의 핵심인 재귀를 썼다는 거에 있어서 DFS라고 볼 수 있겠다.
+
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    string numbers = "0123456789";
-    string full = numbers.substr();
-    string sub = numbers.substr(3, 5);
-    string a = numbers.substr(0);
+int isBalanced(string s);
+bool isProper(string s);
 
-    cout << "Full String: " << full << endl;
-    cout << "Sub String: " << sub << endl;
-    cout << a << '\n';
+string solution(string p) {
+    string answer = "";
+    if (p == "") {
+        return p;
+    }
+    int index = isBalanced(p);
+    string u = p.substr(0, index + 1);
+    string v = p.substr(index + 1);
+
+    if (isProper(u)) {
+        return u + solution(v);
+    }
+    else {
+        string tmp = "(";
+        tmp += solution(v);
+        tmp += ")";
+        u = u.substr(1, u.size() - 2);
+        for (int i = 0; i < u.size(); ++i) {
+            if (u[i] == '(') {
+                u[i] = ')';
+            }
+            else {
+                u[i] = '(';
+            }
+        }
+        tmp += u;
+        return tmp;
+    }
+    //return answer;
+}
+
+int isBalanced(string s) {
+    int count = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '(') {
+            ++count;
+        }
+        else {
+            --count;
+        }
+        if (count == 0) {
+            return i;
+        }
+    }
+
+    // return -1;
+}
+
+bool isProper(string s) {
+    int count = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '(') {
+            ++count;
+        }
+        else {
+            if (count == 0) {
+                return false;
+            }
+            --count;
+        }
+    }
+
+    return true;
 }
