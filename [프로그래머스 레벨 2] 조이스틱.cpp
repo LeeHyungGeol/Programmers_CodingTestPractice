b9bb82ff09356d349aac163836fa9adc251aebbf
LeeHyungGeol <rjf1138@naver.com>
2021-07-06 15:49:00 +0900

    2021-07-06 / 조이스틱 : 그리디

---
+++ [프로그래머스 레벨 2] 조이스틱.cpp
@@ -0,0 +1,38 @@
+// 그리디 문제
+
+// left == i
+// right == ind(i 다음에 있는 A가 아닌 문자)
+// 1) (원점)0 -> i -> 0 -> ind : 2a + b
+// 2) (원점)0 -> ind -> 0 -> i : a + 2b
+// min(2a+b, a+2b) == a+b + min(a,b);
+// a = i, b = n - ind
+// -> step = min(step, i + n - ind + min(i, n - ind));
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(string name) {
+    int answer = 0;
+    int n = name.size();
+    int step = n - 1;
+
+    for (int i = 0; i < name.size(); ++i) {
+        answer += min(name[i] - 'A', 'Z' - name[i] + 1);
+
+        int left = i;
+        int right = i + 1;
+        while (right < n && name[right] == 'A') {
+            ++right;
+        }
+
+        step = min(step, left + n - right + min(left, n - right));
+    }
+
+    answer += step;
+
+    return answer;
+}
