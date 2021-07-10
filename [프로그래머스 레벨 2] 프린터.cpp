34746597bd72f2a7771523a0d8e77cf12831c3cf
LeeHyungGeol <rjf1138@naver.com>
2021-07-10 15:40:40 +0900

    2021-07-10 / 연습문제 : 프린터 : 자료구조, queue, priority_queue

--- [프로그래머스 레벨 2] 프린터.cpp
+++ [프로그래머스 레벨 2] 프린터.cpp
@@ -2,28 +2,43 @@
 #include <vector>
 #include <queue>
 #include <iostream>
+
 using namespace std;
+
 typedef pair<int, int> pii;
+
 int solution(vector<int> priorities, int location) {
     int answer = 0;
+    bool flag = false;
     priority_queue<int> pq;
     queue<pii> q;
+
     for (int i = 0; i < priorities.size(); ++i) {
         pq.push(priorities[i]);
         q.push({ priorities[i], i });
     }
+
     while (!q.empty()) {
-        int num = q.front().first;
-        int idx = q.front().second;
-        q.pop();
-        if (pq.top() == num) {
-            answer++;
-            pq.pop();
-            if (location == idx) break;
+        int priority = pq.top();
+        pii fileSequence = q.front();
+
+        if (priority > fileSequence.first) {
+            q.push(fileSequence);
+            q.pop();
         }
         else {
-            q.push({ num, idx });
+            if (fileSequence.second == location) {
+                flag = true;
+            }
+            pq.pop();
+            q.pop();
+            ++answer;
+        }
+
+        if (flag) {
+            break;
         }
     }
+
     return answer;
 }
