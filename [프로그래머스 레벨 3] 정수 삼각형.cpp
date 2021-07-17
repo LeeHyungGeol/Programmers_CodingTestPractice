2ae0df52eadc513fcdc771d23be715b5fa8e5b20
LeeHyungGeol <rjf1138@naver.com>
2021-07-17 21:42:56 +0900

    2021-07-17 / 연습문제 : 정수 삼각형 : dp(다이나믹 프로그래밍)

---
+++ [프로그래머스 레벨 3] 정수 삼각형.cpp
@@ -0,0 +1,60 @@
+// dp(다이나믹 프로그래밍)
+// (백준 실버 1) 정수 삼각형(1932번) 과 똑같은 문제
+
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int check[501][501];
+// 탑다운 방식(top-down)
+int go(vector<vector<int>>& triangle, int i, int j) {
+    int& ret = check[i][j];
+    if (ret != -1) {
+        return ret;
+    }
+    if (i == triangle.size() - 1) {
+        return triangle[i][j];
+    }
+    return ret = max(go(triangle, i + 1, j + 1) + triangle[i][j], go(triangle, i + 1, j) + triangle[i][j]);
+}
+
+int solution1(vector<vector<int>> triangle) {
+    memset(check, -1, sizeof(check));
+    return go(triangle, 0, 0);
+}
+
+// 바텀업 방식(bottom-up)
+int solution2(vector<vector<int>> triangle) {
+    int answer = triangle[0][0];
+    int n = triangle.size();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < triangle[i].size(); ++j) {
+            int left;
+            int right;
+
+            if (j - 1 < 0) {
+                left = 0;
+            }
+            else {
+                left = triangle[i - 1][j - 1];
+            }
+            if (i - 1 < j) {
+                right = 0;
+            }
+            else {
+                right = triangle[i - 1][j];
+            }
+
+            triangle[i][j] = triangle[i][j] + max(left, right);
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        answer = max(answer, triangle[n - 1][i]);
+    }
+
+    return answer;
+}
