89c13993efb6546a9f84a8d21274f8323d3d70c0
LeeHyungGeol <rjf1138@naver.com>
2021-07-17 18:05:43 +0900

    2021-07-17 / 연습문제 : 가장 큰 정사각형 : dp(다이나믹 프로그래밍)

--- [프로그래머스 레벨 2] 가장 큰 정사각형 찾기.cpp
+++ [프로그래머스 레벨 2] 가장 큰 정사각형 찾기.cpp
@@ -1,3 +1,7 @@
+// dp문제
+// (백준 실버 1) 정수 삼각형(1932번) 과 매우 유사한 문제
+// (백준 골드 5) 가장 큰 정사각형(1915번) 과 똑같은 문제
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,12 +9,14 @@
 using namespace std;
 
 int solution(vector<vector<int>> board) {
-    int answer = 0;
+    int answer = board[0][0];
 
     for (int i = 1; i < board.size(); ++i) {
         for (int j = 1; j < board[0].size(); ++j) {
-            board[i][j] = min(board[i - 1][j - 1], min(board[i][j - 1], board[i - 1][j])) + 1;
-            answer = max(answer, board[i][j]);
+            if (board[i][j]) {
+                board[i][j] = min(board[i - 1][j - 1], min(board[i][j - 1], board[i - 1][j])) + 1;
+                answer = max(answer, board[i][j]);
+            }
         }
     }
 
