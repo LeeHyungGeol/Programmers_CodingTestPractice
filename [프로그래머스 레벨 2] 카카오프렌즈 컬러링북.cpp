28e7cc0de752a62dc444523e70807ddac78e6f24
LeeHyungGeol <rjf1138@naver.com>
2021-08-28 23:06:23 +0900

    2021-08-28 / 2017 카카오코드 예선 : 카카오프렌즈 컬러링북 : dfs/bfs (connected components)

--- [프로그래머스 레벨 2] 카카오프렌즈 컬러링북.cpp
+++ [프로그래머스 레벨 2] 카카오프렌즈 컬러링북.cpp
@@ -4,65 +4,55 @@
 
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
-bool visited[100][100];
 int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
+vector<vector<bool>> Visited;
 
-pii dfsAll(int m, int n, vector<vector<int>> picture);
-int dfs(int x, int y, int curColor, vector<vector<int>> picture);
+vector<int> dfsAll(int m, int n, vector<vector<int>> picture);
+int dfs(int x, int y, int m, int n, vector<vector<int>> picture, int curColor);
 
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
-    pii answers = dfsAll(m, n, picture);
-
-    int number_of_area = answers.first;
-    int max_size_of_one_area = answers.second;
-
-    vector<int> answer(2);
-    answer[0] = number_of_area;
-    answer[1] = max_size_of_one_area;
+    vector<int> answer = dfsAll(m, n, picture);
     return answer;
 }
 
+vector<int> dfsAll(int m, int n, vector<vector<int>> picture) {
+    int number_of_area = 0, max_size_of_one_area = 0;
+    vector<int> result;
 
-pii dfsAll(int m, int n, vector<vector<int>> picture) {
-    int components = 0;
-    int maxConnectedNodes = 0;
-
-    for (int i = 0; i < 100; ++i) {
-        fill(visited[i], visited[i] + 100, false);
-    }
+    Visited.assign(m, vector<bool>(n, false));
 
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (!visited[i][j] && picture[i][j] > 0) {
-                int nodes = dfs(i, j, picture[i][j], picture);
-                ++components;
-                maxConnectedNodes = max(maxConnectedNodes, nodes);
+            if (!Visited[i][j] && picture[i][j] != 0) {
+                int nodes = dfs(i, j, m, n, picture, picture[i][j]);
+                ++number_of_area;
+                max_size_of_one_area = max(max_size_of_one_area, nodes);
             }
         }
     }
 
-    return { components, maxConnectedNodes };
+    result.push_back(number_of_area);
+    result.push_back(max_size_of_one_area);
+
+    return result;
 }
 
-int dfs(int x, int y, int curColor, vector<vector<int>> picture) {
-    visited[x][y] = true;
-
+int dfs(int x, int y, int m, int n, vector<vector<int>> picture, int curColor) {
     int nodes = 1;
+    Visited[x][y] = true;
 
     for (int i = 0; i < 4; ++i) {
         int nx = x + dx[i];
         int ny = y + dy[i];
 
-        if (0 <= nx && nx < picture.size() && 0 <= ny && ny < picture[0].size()) {
-            if (!visited[nx][ny] && curColor == picture[nx][ny]) {
-                nodes += dfs(nx, ny, curColor, picture);
+        if (0 <= nx && nx < m && 0 <= ny && ny < n) {
+            if (!Visited[nx][ny] && picture[nx][ny] == curColor) {
+                nodes += dfs(nx, ny, m, n, picture, picture[nx][ny]);
             }
         }
     }
