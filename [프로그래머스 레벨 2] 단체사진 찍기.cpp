1d6d779686a09635bea9387df7c48e9930e868aa
LeeHyungGeol <rjf1138@naver.com>
2021-08-28 23:43:29 +0900

    2021-08-28 / 2017 카카오코드 본선 : 단체사진 찍기 : dfs, 순열, 구현

--- [프로그래머스 레벨 2] 단체사진 찍기.cpp
+++ [프로그래머스 레벨 2] 단체사진 찍기.cpp
@@ -1,52 +1,60 @@
+// dfs, 순열, 구현
+
 #include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int answer = 0;
-bool Select[8];
-char arr[8] = { 'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T' };
-char cases[8];
+int Answer;
+char Arr[] = { 'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T' };
+vector<char> Cases;
+vector<bool> Select;
 
-// 순열 문제
+void dfs(int cnt, vector<string> data);
+
+int solution(int n, vector<string> data) {
+    Answer = 0;
+    Select.assign(8, false);
+    Cases.assign(8, ' ');
+    dfs(0, data);
+    return Answer;
+}
+
 void dfs(int cnt, vector<string> data) {
     if (cnt == 8) {
         for (int i = 0; i < data.size(); ++i) {
-            char x1 = data[i][0];
-            char x2 = data[i][2];
-            char condition = data[i][3];
-            int dist = data[i][4] - '0';
+            char a = data[i][0];
+            char b = data[i][2];
+            char cond = data[i][3];
+            int diff = data[i][4] - '0';
 
-            int x1Index = -1, x2Index = -1;
+            int aIndex = -1, bIndex = -1;
 
-            for (int j = 0; j < 8; ++j) {
-                if (cases[j] == x1) {
-                    x1Index = j;
+            for (int j = 0; j < Cases.size(); ++j) {
+                if (Cases[j] == a) {
+                    aIndex = j;
                 }
-                if (cases[j] == x2) {
-                    x2Index = j;
+                if (Cases[j] == b) {
+                    bIndex = j;
                 }
-                if (x1Index != -1 && x2Index != -1) {
+                if (aIndex != -1 && bIndex != -1) {
                     break;
                 }
             }
 
-            // 같음
-            if (condition == '=' && abs(x1Index - x2Index) != dist + 1) {
+            if (cond == '=' && abs(aIndex - bIndex) != diff + 1) {
                 return;
             }
-            // 초과
-            if (condition == '<' && abs(x1Index - x2Index) >= dist + 1) {
+            if (cond == '>' && abs(aIndex - bIndex) <= diff + 1) {
                 return;
             }
-            // 미만
-            if (condition == '>' && abs(x1Index - x2Index) <= dist + 1) {
+            if (cond == '<' && abs(aIndex - bIndex) >= diff + 1) {
                 return;
             }
         }
 
-        ++answer;
+        ++Answer;
 
         return;
     }
@@ -56,16 +64,8 @@ void dfs(int cnt, vector<string> data) {
             continue;
         }
         Select[i] = true;
-        cases[cnt] = arr[i];
+        Cases[cnt] = Arr[i];
         dfs(cnt + 1, data);
         Select[i] = false;
     }
-}
-
-int solution(int n, vector<string> data) {
-    answer = 0;
-
-    dfs(0, data);
-
-    return answer;
 }
