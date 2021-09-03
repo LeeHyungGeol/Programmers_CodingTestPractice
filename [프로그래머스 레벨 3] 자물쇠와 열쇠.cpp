45a810c248eaaa1dbf7a3dec347df7486a41687b
LeeHyungGeol <rjf1138@naver.com>
2021-09-03 23:21:36 +0900

    2021-09-03 / 2020 카카오 공채 : 자물쇠와 열쇠 : 2차원 배열의 크기 3배 확대, 2차원 배열 90도 회전 구현

--- [프로그래머스 레벨 3] 자물쇠와 열쇠.cpp
+++ [프로그래머스 레벨 3] 자물쇠와 열쇠.cpp
@@ -1,75 +1,77 @@
+// 2차원 배열의 크기 3배 확대, 2차원 배열 90도 회전 구현
+
 // https://dev-mystory.tistory.com/227
 // https://velog.io/@domybest/2%EC%B0%A8%EC%9B%90-%EB%B0%B0%EC%97%B4-%ED%9A%8C%EC%A0%84%EC%8B%9C%ED%82%A4%EA%B8%B0
 
-#include <vector>
 #include <string>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
-// 시계방향 회전
-vector<vector<int>> rotate90degree(vector<vector<int>> arr) {
-    int n = arr.size();
-    int m = arr[0].size();
-    vector<vector<int>> result(n, vector<int>(m));
+vector<vector<int>> rotate(vector<vector<int>> arr);
+bool check(vector<vector<int>> lock);
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+    int n = lock.size();
+    int m = key.size();
+    vector<vector<int>> newLock(n * 3, vector<int>(n * 3, 0));
 
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            result[j][n - i - 1] = arr[i][j];
+        for (int j = 0; j < n; ++j) {
+            newLock[n + i][n + j] = lock[i][j];
         }
     }
 
-    return result;
+    for (int r = 0; r < 4; ++r) {
+        key = rotate(key);
+
+        for (int i = 0; i < n * 2; ++i) {
+            for (int j = 0; j < n * 2; ++j) {
+                for (int x = 0; x < m; ++x) {
+                    for (int y = 0; y < m; ++y) {
+                        newLock[i + x][j + y] += key[x][y];
+                    }
+                }
+
+                if (check(newLock)) {
+                    return true;
+                }
+
+                for (int x = 0; x < m; ++x) {
+                    for (int y = 0; y < m; ++y) {
+                        newLock[i + x][j + y] -= key[x][y];
+                    }
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
-bool check(vector<vector<int>> bigLock) {
-    int realLockSize = bigLock.size() / 3;
+vector<vector<int>> rotate(vector<vector<int>> arr) {
+    int n = arr.size();
+    int m = arr[0].size();
+    vector<vector<int>> temp(n, vector<int>(m, 0));
 
-    for (int i = realLockSize; i < realLockSize * 2; ++i) {
-        for (int j = realLockSize; j < realLockSize * 2; ++j) {
-            if (bigLock[i][j] != 1) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            temp[j][n - i - 1] = arr[i][j];
+        }
+    }
+
+    return temp;
+}
+
+bool check(vector<vector<int>> lock) {
+    int n = lock.size() / 3;
+
+    for (int i = n; i < n * 2; ++i) {
+        for (int j = n; j < n * 2; ++j) {
+            if (lock[i][j] != 1) {
                 return false;
             }
         }
     }
-
     return true;
-}
-
-bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
-    int N = lock.size();
-    int M = key.size();
-
-    vector<vector<int>> bigLock(N * 3, vector<int>(N * 3));
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            bigLock[i + N][j + N] = lock[i][j];
-        }
-    }
-
-    for (int rotation = 0; rotation < 4; ++rotation) {
-        key = rotate90degree(key);
-        for (int i = 0; i < N * 2; ++i) {
-            for (int j = 0; j < N * 2; ++j) {
-
-                for (int x = 0; x < M; ++x) {
-                    for (int y = 0; y < M; ++y) {
-                        bigLock[i + x][j + y] += key[x][y];
-                    }
-                }
-
-                if (check(bigLock)) {
-                    return true;
-                }
-
-                for (int x = 0; x < M; ++x) {
-                    for (int y = 0; y < M; ++y) {
-                        bigLock[i + x][j + y] -= key[x][y];
-                    }
-                }
-            }
-        }
-    }
-    return false;
 }
