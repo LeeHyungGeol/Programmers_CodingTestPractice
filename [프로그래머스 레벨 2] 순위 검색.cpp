98dd477ae4e560dae3ccd23f022220085bdc3a9a
LeeHyungGeol <rjf1138@naver.com>
2021-09-07 11:12:10 +0900

    2021-09-07 / 2021 카카오 공채 : 순위 검색 : Map<string, vector<string>>, 부분집합(Bitmask), 이진탐색(lower_bound)

--- [프로그래머스 레벨 2] 순위 검색.cpp
+++ [프로그래머스 레벨 2] 순위 검색.cpp
@@ -1,29 +1,30 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <sstream>
+#include <map>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 map<string, vector<int>> Map;
+stringstream SS;
 
 vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
 
     for (string str : info) {
-        vector<vector<string>> temp(4, vector<string>(2, "-"));
-        stringstream ss(str);
-        string a, b, c, d;
+        SS.str(str);
+        string lang, job, career, food;
         int score;
 
-        ss >> a >> b >> c >> d >> score;
+        SS >> lang >> job >> career >> food >> score;
 
-        temp[0][0] = a;
-        temp[1][0] = b;
-        temp[2][0] = c;
-        temp[3][0] = d;
+        vector<vector<string>> temp(4, vector<string>(2, "-"));
+
+        temp[0][0] = lang;
+        temp[1][0] = job;
+        temp[2][0] = career;
+        temp[3][0] = food;
 
         for (int i = 0; i < 2; ++i) {
             string t1, t2, t3, t4;
@@ -32,37 +33,122 @@ vector<int> solution(vector<string> info, vector<string> query) {
                 t2 = temp[1][j];
                 for (int k = 0; k < 2; ++k) {
                     t3 = temp[2][k];
-                    for (int l = 0; l < 2; ++l) {
-                        t4 = temp[3][l];
+                    for (int u = 0; u < 2; ++u) {
+                        t4 = temp[3][u];
                         Map[t1 + t2 + t3 + t4].push_back(score);
                     }
                 }
             }
         }
+
+        SS.clear();
     }
 
-    /*for (auto itr = Map.begin(); itr != Map.end(); ++itr) {
-        sort(itr->second.begin(), itr->second.end());
-    }*/
-
+    // 조회가 아닐 때는 &기호 붙여주자.
     for (auto& m : Map) {
         sort(m.second.begin(), m.second.end());
     }
 
-    for (string q : query) {
-        string a, b, c, d, temp;
+    for (string str : query) {
+        SS.str(str);
+        string lang, job, career, food, temp;
         int score;
-        stringstream ss(q);
 
-        ss >> a >> temp >> b >> temp >> c >> temp >> d >> score;
+        SS >> lang >> temp >> job >> temp >> career >> temp >> food >> score;
 
-        string tmp = a + b + c + d;
+        string s = lang + job + career + food;
 
-        // auto low = lower_bound(Map[tmp].begin(), Map[tmp].end(), score);
-        vector<int>::iterator low = lower_bound(Map[tmp].begin(), Map[tmp].end(), score);
+        int index = lower_bound(Map[s].begin(), Map[s].end(), score) - Map[s].begin();
 
-        answer.push_back(Map[tmp].end() - low);
+        answer.push_back(Map[s].size() - index);
+
+        SS.clear();
     }
 
     return answer;
+}
+
+//-----------------------2번째 풀이----------------------//
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+stringstream SS;
+map<string, int> MAP;
+vector<int> Combinations[4][3][3][3];
+
+void makeMap();
+
+vector<int> solution(vector<string> info, vector<string> query) {
+    vector<int> answer;
+
+    makeMap();
+
+    for (string str : info) {
+        SS.str(str);
+        string lang, job, career, food;
+        int score;
+
+        SS >> lang >> job >> career >> food >> score;
+
+        int arr[4] = { MAP[lang], MAP[job], MAP[career], MAP[food] };
+
+        for (int i = 0; i < (1 << 4); ++i) {
+            int index[4] = { 0, };
+            for (int j = 0; j < 4; ++j) {
+                if (i & (1 << j)) {
+                    index[j] = arr[j];
+                }
+            }
+            Combinations[index[0]][index[1]][index[2]][index[3]].push_back(score);
+        }
+
+        SS.clear();
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                for (int u = 0; u < 3; ++u) {
+                    sort(Combinations[i][j][k][u].begin(), Combinations[i][j][k][u].end());
+                }
+            }
+        }
+    }
+
+    for (string str : query) {
+        SS.str(str);
+        string lang, job, career, food, And;
+        int score;
+
+        SS >> lang >> And >> job >> And >> career >> And >> food >> score;
+
+        auto& temp = Combinations[MAP[lang]][MAP[job]][MAP[career]][MAP[food]];
+
+        int index = lower_bound(temp.begin(), temp.end(), score) - temp.begin();
+
+        answer.push_back(temp.size() - index);
+
+        SS.clear();
+    }
+
+    return answer;
+}
+
+void makeMAP() {
+    MAP["-"] = 0;
+    MAP["cpp"] = 1;
+    MAP["java"] = 2;
+    MAP["python"] = 3;
+    MAP["backend"] = 1;
+    MAP["frontend"] = 2;
+    MAP["junior"] = 1;
+    MAP["senior"] = 2;
+    MAP["chicken"] = 1;
+    MAP["pizza"] = 2;
 }
