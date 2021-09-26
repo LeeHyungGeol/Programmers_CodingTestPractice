827d5361803062fe23284125f96ea5d08612cd27
LeeHyungGeol <rjf1138@naver.com>
2021-09-26 23:52:19 +0900

    2021-09-26 / 거스름돈 : dp(동적계획법)

---
+++ [프로그래머스 레벨 3] 거스름돈.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+#define MOD 1000000007
+
+using namespace std;
+
+int solution(int n, vector<int> money) {
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+
+    for (int i = 0; i < money.size(); ++i) {
+        for (int j = money[i]; j <= n; ++j) {
+            dp[j] += dp[j - money[i]] % MOD;
+        }
+    }
+
+    return dp[n];
+}
