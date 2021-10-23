b47c95e09fda883e5beef87831175f2dcb12237b
LeeHyungGeol <rjf1138@naver.com>
2021-10-24 03:43:28 +0900

    2021-10-24 / 타겟 넘버 : dfs,재귀,백트랙킹

--- [프로그래머스 레벨 2] 타겟 넘버.cpp
+++ [프로그래머스 레벨 2] 타겟 넘버.cpp
@@ -4,25 +4,28 @@
 
 // (백준 실버 1) 14888번 연산자 끼워넣기 와 매우 유사한 문제 
 
+// 3번째 푼다.
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int answer = 0;
-
-void dfs(vector<int> numbers, int index, int total, int target) {
-    if (index == numbers.size()) {
-        if (total == target)
-            ++answer;
-        return;
-    }
-
-    dfs(numbers, index + 1, total + numbers[index], target);
-    dfs(numbers, index + 1, total - numbers[index], target);
-}
+int Answer;
+void dfs(vector<int> numbers, int target, int sum, int index);
 
 int solution(vector<int> numbers, int target) {
-    dfs(numbers, 0, 0, target);
-    return answer;
+    dfs(numbers, target, 0, 0);
+    return Answer;
+}
+
+void dfs(vector<int> numbers, int target, int sum, int index) {
+    if (index == numbers.size()) {
+        if (sum == target) {
+            ++Answer;
+        }
+        return;
+    }
+    dfs(numbers, target, sum + numbers[index], index + 1);
+    dfs(numbers, target, sum - numbers[index], index + 1);
 }
