94ef02723c81c389e57afb4630e23c216836f1a2
LeeHyungGeol <rjf1138@naver.com>
2021-11-02 03:05:28 +0900

    2021-11-02 / 2018 카카오 공채 : [1차]비밀지도 : 구현,문자열, bit연산

---
+++ [프로그래머스 레벨 1] [1차]비밀지도.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string toBinary(int n, int size);
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+
+    for (int i = 0; i < n; ++i) {
+        answer.push_back(toBinary(arr1[i] | arr2[i], n));
+    }
+
+    return answer;
+}
+
+string toBinary(int n, int size) {
+    string result = "";
+    while (n) {
+        result += (n % 2) ? '#' : ' ';
+        n /= 2;
+    }
+
+    while (result.length() < size) {
+        result += ' ';
+    }
+
+    reverse(result.begin(), result.end());
+
+    return result;
+}
