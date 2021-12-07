6708293db4cacd68861ffb8c1911d4fa2a5c13fe
LeeHyungGeol <rjf1138@naver.com>
2021-12-07 11:14:38 +0900

    2021-12-07 / 2020 카카오 공채 : 괄호 변환 : 구현,재귀,dfs

--- [프로그래머스 레벨 2] 괄호 변환.cpp
+++ [프로그래머스 레벨 2] 괄호 변환.cpp
@@ -1,78 +1,49 @@
-// 구현, dfs(재귀 사용) 문제
-
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int isBalanced(string str);
-bool isProper(string str);
+int isBalanced(string s);
+bool isProper(string s);
 
 string solution(string p) {
     string answer = "";
 
-    if (p.empty()) {
-        return answer;
-    }
-
+    if (p.empty()) return p;
     int index = isBalanced(p);
     string u = p.substr(0, index + 1);
     string v = p.substr(index + 1);
-
     if (isProper(u)) {
         return u + solution(v);
     }
     else {
-        answer = "(";
-        answer += solution(v);
-        answer += ")";
+        answer += "(" + solution(v) + ")";
         u.erase(u.begin());
         u.pop_back();
-
-        for (int i = 0; i < u.length(); ++i) {
-            if (u[i] == '(') {
-                u[i] = ')';
-            }
-            else {
-                u[i] = '(';
-            }
-        }
+        for (int i = 0; i < u.length(); ++i) u[i] = (u[i] == '(') ? ')' : '(';
         answer += u;
-    }
-
-    return answer;
-}
-
-int isBalanced(string str) {
-    int cnt = 0;
-
-    for (int i = 0; i < str.length(); ++i) {
-        if (str[i] == '(') {
-            ++cnt;
-        }
-        else {
-            --cnt;
-        }
-        if (cnt == 0) {
-            return i;
-        }
+        return answer;
     }
 }
 
-bool isProper(string str) {
+int isBalanced(string s) {
     int cnt = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '(') ++cnt;
+        else --cnt;
+        if (cnt == 0) return i;
+    }
+    return -1;
+}
 
-    for (int i = 0; i < str.length(); ++i) {
-        if (str[i] == '(') {
-            ++cnt;
-        }
+bool isProper(string s) {
+    int cnt = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '(') ++cnt;
         else {
             --cnt;
-            if (cnt == 0) {
-                return true;
-            }
+            if (cnt == 0) return true;
         }
     }
-
     return false;
 }
