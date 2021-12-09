4357da847cfd4b88606e7ab6a3feeb586c2027fb
LeeHyungGeol <rjf1138@naver.com>
2021-12-09 21:36:23 +0900

    2021-12-09 / 더 맵게 : heap, priority_queue

--- [프로그래머스 레벨 2] 더 맵게.cpp
+++ [프로그래머스 레벨 2] 더 맵게.cpp
@@ -1,47 +1,36 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <iostream>
 
 using namespace std;
 
-// 항상 최소 값을 알 수 있어야 하므로 heap(priority_queue)를 사용한다.
+typedef struct Node {
+    int x;
+
+    bool operator < (const Node& other) const {
+        return x > other.x;
+    }
+}Node;
+
+priority_queue<Node> PQ;
+
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    priority_queue<int> pq;
 
-    for (int i = 0; i < scoville.size(); ++i) {
-        pq.push(-scoville[i]);
+    for (int s : scoville) {
+        PQ.push({ s });
     }
 
-    bool flag = false;
-
-    while (true) {
-        // 가장 위의 값을 바로 알기 위해 pq.top()을 사용한다.
-        if (-pq.top() >= K) {
-            break;
-        }
-        // 맛을 조합하기 위해서는 pq에 최소한 2개의 값이 필요하므로 pq.size()를 이용하여
-        // pq의 크기를 구한다.
-        if (pq.size() < 2) {
-            flag = true;
-            break;
-        }
-
-        int Min = -pq.top();
-        pq.pop();
-
-        int secondMin = -pq.top();
-        pq.pop();
-
-        int newMin = Min + 2 * secondMin;
-
-        pq.push(-newMin);
-
+    while (!PQ.empty()) {
+        if (PQ.top().x >= K) break;
+        if (PQ.size() < 2) return answer = -1;
+        int first = PQ.top().x;
+        PQ.pop();
+        int second = PQ.top().x;
+        PQ.pop();
+        PQ.push({ first + 2 * second });
         ++answer;
-    }
-
-    if (flag) {
-        return -1;
     }
 
     return answer;
